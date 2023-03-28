c3f96980c5d80b4abc34c2b8666cc50b2eaec825
Kanako <kanakoyano.code@gmail.com>
2023-03-28 20:49:48 +1100

    q

--- website/src/App.js
+++ website/src/App.js
@@ -2,14 +2,19 @@ import { firebaseConfig } from "./config/Config";
 import { initializeApp } from "firebase/app";
 // import config from "./config/Config";
 import "./App.css";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Routes, Route } from "react-router-dom";
 
-import { Test } from "./components/Test";
 const FirebaseApp = initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <Test />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
