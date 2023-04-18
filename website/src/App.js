f71c90997eeca0cba16be1e8da6932def710104b
Kanako <kanakoyano.code@gmail.com>
2023-04-18 18:49:40 +1000

    Added Header to home page

--- website/src/App.js
+++ website/src/App.js
@@ -5,15 +5,18 @@ import "./App.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Routes, Route } from "react-router-dom";
-
+import { Contact } from "./pages/Contact";
+import { Header } from "./components/Header";
 const FirebaseApp = initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
   );
---
+++ website/src/components/Header.js
@@ -0,0 +1,13 @@
+import { NavbarBrand } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+export function Header(props) {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand>Hello Website</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+}
