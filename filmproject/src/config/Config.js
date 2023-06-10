a1a7c5d7bf18f94b9954cc7987f828c43f03aa00
Kanako <kanakoyano.code@gmail.com>
2023-06-10 11:43:00 +1000

    Add firebase to the app

---
+++ filmproject/src/config/Config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDu2Uji2ScAJLEGl99ni3N5dlR-KPK8EX0",
+  authDomain: "filmstackoverview.firebaseapp.com",
+  projectId: "filmstackoverview",
+  storageBucket: "filmstackoverview.appspot.com",
+  messagingSenderId: "625880847547",
+  appId: "1:625880847547:web:c5ea34300dbd4b8c90c59e",
+  measurementId: "G-Z0HTN78X2K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
